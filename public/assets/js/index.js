5f1da7575fedc603a5debd5f993b669b69812c72
TimK76 <haadelphos2001@gmail.com>
2022-04-20 14:37:28 -0400

    file structure edit

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>');
+});
 const PORT = process.env || 3001;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
