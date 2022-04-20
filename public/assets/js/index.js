760ef61946242e21cb70fb80b3370344d4c3c44d
TimK76 <haadelphos2001@gmail.com>
2022-04-20 17:09:47 -0400

    created first route

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,14 +1,3 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World</h1>');
-});
-const PORT = process.env || 3001;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-
 let noteTitle;
 let noteText;
 let saveNoteBtn;
